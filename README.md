0b5c18ab5a3e6c0d78c2ea3467c19fb795332837
Patrick Melix <patrick.melix@uni-leipzig.de>
2020-07-17 09:56:15 +0200

    dcd now included in ASE

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+*DEPRECEATED*
+Reading DCD files is now implemented in ASE. This is therefore not needed anymore. Updated the script to use the new ASE
+features.
+
 # dcd2ase
 Convert DCD Trajectories to ASE Atoms. If called as a script convert xyz and dcd input into one ext-xyz trajectory file.
 
 Requirements:
-* pwtools: https://github.com/elcorto/pwtools (do not use pip install, this will give you a different package!!)
+* ~~pwtools: https://github.com/elcorto/pwtools (do not use pip install, this will give you a different package!!)~~
 * ASE: https://wiki.fysik.dtu.dk/ase/
 * Also using: os, argparse
--- dcd2ase.py
+++ dcd2ase.py
@@ -1,75 +1,40 @@
-#!/usr/bin/env python2
-# Convert/Read dcd files to ASE Atoms
+#!/usr/bin/env python3
+# Convert/Read dcd files to ASE Atoms and write as xyz
 #
 #
-# Requires: pwtools, ase, os
-#
-# pwtools from https://github.com/elcorto/pwtools
-# WARNING: pip install pwtools installs something different!!
+# Requires: ase, os
 #
 #
-# by: Patrick Melix, 2018
+#
+# by: Patrick Melix, 2019
 
 
-from pwtools import dcd
-from ase import Atoms
-from ase import io as aseIO
+from ase import Atoms, io
+from ase.io.cp2k import iread_cp2k_dcd
 import os, argparse
 
 
-def _combine2Atoms(cellsIn, coordsIn, elements):
-    #make that cell angles and lengths are complete
-    lenSet = set([ len(x) for x in cellsIn ])
-    assert len(lenSet) == 1
-    assert 6 in lenSet
-    #compare number of cells and coords
-    assert len(cellsIn) == len(coordsIn)
-    #compare nAtoms
-    lenSet = set([ len(x) for x in coordsIn ])
-    assert len(lenSet) == 1
-    assert len(elements) in lenSet
-
-    #convert
-    traj = []
-    for i, coordsI in enumerate(coordsIn):
-        traj.append(Atoms(elements, coordsI))
-        traj[-1].set_cell(cellsIn[i])
-    return traj
-
-
-
-
 def dcd2ase(dcdFile,refStructFile):
-    cellsIn, coordsIn = dcd.read_dcd_data(dcdFile)
-    refStruct = aseIO.read(refStructFile)
+    ref = io.read(refStructFile)
+    with open(dcdFile, 'rb') as f:
+        idcd = iread_cp2k_dcd(f, indices=slice(0,None), ref_atoms=ref, aligned=True)
+        for image in idcd:
+            yield image
 
-    #make sure number of atoms is the same for all structures
-    lenSet = set([ len(x) for x in coordsIn ])
-    assert len(lenSet) == 1
-    assert len(refStruct) in lenSet
-
-    #list of element symbols from reference
-    elements = refStruct.get_chemical_symbols()
-    assert len(elements) == len(refStruct)
-
-    return _combine2Atoms(cellsIn, coordsIn, elements)
 
 
 
 
 def _writeExtXYZ(traj, outFile='traj.xyz'):
     #write to xyz
-    if isinstance(outFile,basestring):
+    if isinstance(outFile, str):
         out = open(outFile, mode='w')
     else:
         out = outFile
-    if len(traj) == 1:
-        return
     for frame in traj:
+        frame.wrap()
         frame.write(out, format='xyz', append=True)
     return
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert XYZ and DCD input to ASE-extxyz trajectory')
@@ -77,5 +42,4 @@ if __name__ == "__main__":
     parser.add_argument('indcd', type=str, help='input dcd file')
     parser.add_argument('output', type=argparse.FileType('w'), help='output file')
     args = parser.parse_args()
-    traj = dcd2ase(args.indcd,args.inxyz)
-    _writeExtXYZ(traj,args.output)
+    _writeExtXYZ(dcd2ase(args.indcd,args.inxyz),args.output)
